e93115bdc8237f03d9ca25431a0831d49e3a3b27
Josh Long <josh@joshlong.com>
2016-01-29 04:34:20 -0800

    adding dr syer's brilliance

---
+++ authserver/src/main/java/demo/SimpleUserDetailsService.java
@@ -0,0 +1,43 @@
+package demo;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
+
+/**
+ * @author Josh Long
+ */
+public class SimpleUserDetailsService implements UserDetailsService {
+
+	private final Map<String, User> users = new ConcurrentHashMap<>();
+
+	public SimpleUserDetailsService() {
+		List<GrantedAuthority> grantedAuthorities = Arrays.asList("openid", "read", "write")
+				.stream()
+				.map(SimpleGrantedAuthority::new)
+				.collect(Collectors.toList());
+
+		Arrays.asList("jlong", "lshannon", "dsyer", "sgibb")
+				.stream()
+				.map(x -> new User(x, "password", grantedAuthorities))
+				.forEach(u -> this.users.put(u.getUsername(), u));
+
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		if (!this.users.containsKey(username))
+			throw new UsernameNotFoundException("couldn't find user " + username + "!");
+
+		return this.users.get(username);
+	}
+}
