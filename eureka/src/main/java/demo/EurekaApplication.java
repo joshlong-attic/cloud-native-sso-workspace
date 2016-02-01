747ac4357221c03eed47c51f535607816cf91356
Josh Long <josh@joshlong.com>
2016-01-31 17:18:01 -0800

    second cut

--- eureka/src/main/java/demo/EurekaApplication.java
+++ eureka/src/main/java/demo/EurekaApplication.java
@@ -2,13 +2,14 @@ package demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EnableEurekaServer
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaApplication {
 
-	public static void main(String[] args) {
+	public static void main(String args[]) {
 		SpringApplication.run(EurekaApplication.class, args);
 	}
 }
